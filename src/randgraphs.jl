5ae0de741d63bd9e8577b5e32f4763e533fa9e03
Marc Liberatore <liberato@cs.umass.edu>
2015-07-16 13:49:16 -0400

    add a random regular graph generator

--- src/randgraphs.jl
+++ src/randgraphs.jl
@@ -81,3 +81,80 @@ function watts_strogatz(n::Integer, k::Integer, β::Real; is_directed=false)
     end
     return g
 end
+
+function random_regular_graph(n::Integer, k::Integer, seed=-1)
+    @assert(n * k % 2 == 0, "n * k must be even")
+    @assert(0 <= k < n, "the 0 <= k < n inequality must be satisfied")
+    if k == 0
+        return Graph(n)
+    end
+    if seed >= 0
+        srand(seed)
+    end
+    
+    function suitable(edges, potential_edges)
+        if isempty(potential_edges)
+            return true
+        end
+        ends = collect(keys(potential_edges))
+        for i in 1:length(ends)
+            for j in i+1:length(ends)
+                s1, s2 = ends[i:i+1]
+                if s1 > s2
+                    s1, s2 = s2, s1
+                end
+                if ∉((s1, s2), edges)
+                    return true
+                end
+            end
+        end
+        return false
+    end
+    
+    function try_creation()
+        edges = Set()
+        stubs = []
+        for _ in 1:k
+            stubs = vcat(stubs, [1:n])
+        end
+        
+        while !isempty(stubs)
+            potential_edges =  Dict{Int,Int}()
+            shuffle!(stubs)
+            for i in 1:2:length(stubs)
+                s1,s2 = stubs[i:i+1]
+                if s1 > s2
+                    s1, s2 = s2, s1
+                end
+                if s1 != s2 && ∉((s1, s2), edges)
+                    union!(edges, [(s1, s2)])
+                else
+                    potential_edges[s1] = get(potential_edges, s1, 0) + 1
+                    potential_edges[s2] = get(potential_edges, s2, 0) + 1
+                end
+            end
+            
+            if !suitable(edges, potential_edges)
+                return Set()
+            end
+            
+            stubs = []
+            for (e, ct) in potential_edges
+                stubs = vcat(stubs, fill(e, ct))
+            end
+        end
+        return edges
+    end
+    
+    edges = try_creation()
+    while isempty(edges)
+        edges = try_creation()
+    end
+    
+    G = Graph(n)
+    for edge in edges
+        add_edge!(G, edge...)
+    end
+    
+    return G
+end
