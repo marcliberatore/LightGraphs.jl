0b473917e9c84953957c5ee23087afc5c2b77bcb
Seth Bromberger <sbromberger@users.noreply.github.com>
2015-07-16 21:13:27 -0700

    Update randgraphs.jl
    optimized per https://gist.github.com/sbromberger/30e26d49d1e306acf633

--- src/randgraphs.jl
+++ src/randgraphs.jl
@@ -82,8 +82,58 @@ function watts_strogatz(n::Integer, k::Integer, β::Real; is_directed=false)
     return g
 end
 
-function random_regular_graph(n::Integer, k::Integer, seed=-1)
-    @assert(n * k % 2 == 0, "n * k must be even")
+function _suitable(edges::Set{Edge}, potential_edges::Dict{Int, Int})
+    isempty(potential_edges) && return true
+    ends = collect(keys(potential_edges))
+    for i in 1:length(ends)
+        for j in i+1:length(ends)
+            s1, s2 = ends[i:i+1]
+            if s1 > s2
+                s1, s2 = s2, s1
+            end
+            if ∉(Edge(s1, s2), edges)
+                return true
+            end
+        end
+    end
+    return false
+end
+
+function _try_creation(n::Int, k::Int)
+    edges = Set{Edge}()
+    stubs = repmat([1:n;], k)
+
+    while !isempty(stubs)
+        potential_edges =  Dict{Int,Int}()
+        shuffle!(stubs)
+        for i in 1:2:length(stubs)
+            s1,s2 = stubs[i:i+1]
+            if s1 > s2
+                s1, s2 = s2, s1
+            end
+            e = Edge(s1, s2)
+            if s1 != s2 && ∉(e, edges)
+                push!(edges, e)
+            else
+                potential_edges[s1] = get(potential_edges, s1, 0) + 1
+                potential_edges[s2] = get(potential_edges, s2, 0) + 1
+            end
+        end
+
+        if !_suitable(edges, potential_edges)
+            return Set{Edge}()
+        end
+
+        stubs = @compat(Vector{Int}())
+        for (e, ct) in potential_edges
+            append!(stubs, fill(e, ct))
+        end
+    end
+    return edges
+end
+
+function random_regular_graph(n::Int, k::Int, seed::Int=-1)
+    @assert(iseven(n*k), "n * k must be even")
     @assert(0 <= k < n, "the 0 <= k < n inequality must be satisfied")
     if k == 0
         return Graph(n)
@@ -91,70 +141,15 @@ function random_regular_graph(n::Integer, k::Integer, seed=-1)
     if seed >= 0
         srand(seed)
     end
-    
-    function suitable(edges, potential_edges)
-        if isempty(potential_edges)
-            return true
-        end
-        ends = collect(keys(potential_edges))
-        for i in 1:length(ends)
-            for j in i+1:length(ends)
-                s1, s2 = ends[i:i+1]
-                if s1 > s2
-                    s1, s2 = s2, s1
-                end
-                if ∉((s1, s2), edges)
-                    return true
-                end
-            end
-        end
-        return false
-    end
-    
-    function try_creation()
-        edges = Set()
-        stubs = []
-        for _ in 1:k
-            stubs = vcat(stubs, [1:n])
-        end
-        
-        while !isempty(stubs)
-            potential_edges =  Dict{Int,Int}()
-            shuffle!(stubs)
-            for i in 1:2:length(stubs)
-                s1,s2 = stubs[i:i+1]
-                if s1 > s2
-                    s1, s2 = s2, s1
-                end
-                if s1 != s2 && ∉((s1, s2), edges)
-                    union!(edges, [(s1, s2)])
-                else
-                    potential_edges[s1] = get(potential_edges, s1, 0) + 1
-                    potential_edges[s2] = get(potential_edges, s2, 0) + 1
-                end
-            end
-            
-            if !suitable(edges, potential_edges)
-                return Set()
-            end
-            
-            stubs = []
-            for (e, ct) in potential_edges
-                stubs = vcat(stubs, fill(e, ct))
-            end
-        end
-        return edges
-    end
-    
-    edges = try_creation()
+
+    edges = _try_creation(n,k)
     while isempty(edges)
-        edges = try_creation()
+        edges = _try_creation(n,k)
     end
-    
-    G = Graph(n)
+
+    g = Graph(n)
     for edge in edges
-        add_edge!(G, edge...)
+        add_edge!(g, edge)
     end
-    
-    return G
+    return g
 end
